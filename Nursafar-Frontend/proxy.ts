0ac7a2f115f3e1c908f881713d6b241ea3e80b95
shahobiddinzodaoo6-wq <shahobiddinzoda.oo6@gmail.com>
2026-05-05 11:21:59 +0500

    nursafar-project-full

--- Nursafar-Frontend/proxy.ts
+++ Nursafar-Frontend/proxy.ts
@@ -1,7 +1,11 @@
 import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
 
-export default createMiddleware(routing);
+const intlProxy = createMiddleware(routing);
+
+export function proxy(request: Request) {
+  return intlProxy(request as any);
+}
 
 export const config = {
   matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"],
